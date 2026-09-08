0efd8a8a74e9a5917ae6413acd7ab5b7ed358df1
Deepanshu <deepanshu112000singh@gmail.com>
2026-09-08 19:01:15 +0530

    find a number raised to the power of its REVERSE

--- reverse_power.c
+++ reverse_power.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 int main() {
-    int n;
+    int n,rev=0,r,i;
     printf("ENTER A NUMBER : ");
     scanf("%d",&n);
+    for(i=n;i>0;i/=10){
+        r=i%10;
+        rev=rev*10+r;
+    }
+    long long p=(long long)pow(n,rev);
+    printf("%d RAISED TO THE POWER %d IS %d", n, rev, p);
      return 0;
 }
